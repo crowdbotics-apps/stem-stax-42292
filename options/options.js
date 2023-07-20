e376bd05eda262484d5bdc9fc131a19db7783cc3
Crowdbotics <team@crowdbotics.com>
2023-07-20 04:22:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "stem-stax-42292",
+  url: "https://stem-stax-42292.botics.co",
+  api: "https://stem-stax-42292.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
